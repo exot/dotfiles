880db2d7a382b6468551c747618dabb09add7029
Daniel Borchmann <daniel@algebra20.de>
2020-08-23 12:10:29 +0200

    Make custom shell scripts shellcheck clean

--- config/bash/scripts.sh
+++ config/bash/scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function do-startx() {
-  startx $@ >| ~/.xsession-log 2>&1 &
+  startx "$@" >| ~/.xsession-log 2>&1 &
   sleep 1
   vlock
 }
@@ -10,8 +10,10 @@ function encode-with-ffmpeg() {
   movie=$1
   output=$2
 
-  ffmpeg -y -i  $movie -c:v libx264 -preset veryslow -b:v 630k -pass 1 -c:a aac -b:a 128k -f mp4 /dev/null && \
-    ffmpeg -i $movie -c:v libx264 -preset veryslow -b:v 630k -pass 2 -c:a aac -b:a 128k $output
+  ffmpeg -y -i "$movie" -c:v libx264 -preset veryslow -b:v 630k -pass 1 -c:a aac -b:a 128k -f mp4 /dev/null && \
+    ffmpeg -i "$movie" -c:v libx264 -preset veryslow -b:v 630k -pass 2 -c:a aac -b:a 128k "$output"
+}
+
 function run-remote-apt-upgrade() {
   host=$1
 
