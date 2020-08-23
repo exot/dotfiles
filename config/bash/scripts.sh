a709a2145f6e2a6866a8d3d4ce348aab4fbedb19
Daniel Borchmann <daniel@algebra20.de>
2020-08-23 12:09:49 +0200

    Add simple shell function to remotely run apt updates
    This is a very simple function just to save some typing.  No fancy logic is
    included in here, and that's deliberate.

--- config/bash/scripts.sh
+++ config/bash/scripts.sh
@@ -12,4 +12,8 @@ function encode-with-ffmpeg() {
 
   ffmpeg -y -i  $movie -c:v libx264 -preset veryslow -b:v 630k -pass 1 -c:a aac -b:a 128k -f mp4 /dev/null && \
     ffmpeg -i $movie -c:v libx264 -preset veryslow -b:v 630k -pass 2 -c:a aac -b:a 128k $output
+function run-remote-apt-upgrade() {
+  host=$1
+
+  ssh "$host" -t "tmux new-session -d -A -s apt-updates \; send-keys 'sudo apt clean && sudo apt update && sudo apt upgrade && sudo apt autoremove && tripwire --check --interactive' ENTER \; attach"
 }
